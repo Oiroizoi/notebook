4d1c081da01f24c81a721031be5e0fde00995268
Oley Birkeland <john2022026@live.ytech.edu>
2021-12-30 18:36:45 -0500

    Notebooks 7-9 & a few other changes

--- Testing/main.js
+++ Testing/main.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/page', (req, res) => {
+app.get('/alt', (req, res) => {
   res.send('Different text');
 });
 
@@ -26,12 +27,31 @@ app.get('/form', (req, res) => {
 });
 
 app.post('/formdata', (req, res) => {
-  let stringsObj = {
-    input: req.body.string,
-    textarea: req.body.string2
-  };
-  let stringsJSON = JSON.stringify(stringsObj);
   res.render('index', {
     bodytext: `${req.body.string}<br>${req.body.string2}`
+  });
+});
+
+let obj = {
+  string: 'value',
+  number: 10
+};
+obj.number2 = 100;
+let key = 'string';
+obj[key] = 'newValue';
+for (let property in obj) console.log(`${property}: ${obj[property]}`);
+
+let arr = ['first', 'second', 'third'];
+arr.push('fourth');
+for (let item of arr) console.log(item);
+
+let stringObj = JSON.stringify(obj);
+fs.writeFile('data.json', stringObj, () => console.log('Wrote to file'));
+
+app.get('/jsondata', (req, res) => {
+  let fileData = fs.readFileSync('data.json', 'utf8');
+  let parsedData = JSON.parse(fileData);
+  res.render('jsondata', {
+    data: parsedData
   });
 });
